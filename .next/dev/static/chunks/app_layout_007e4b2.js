bc0e3245984a3ed39c659dfa03ac6a7a47c34fad
Krishna Gupta <krishnagupta30mar@gmail.com>
2026-06-27 02:56:33 +0530

    new Project

--- .next/dev/static/chunks/app_layout_007e4b2.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__1e-iwqf._.css","static/chunks/node_modules_next_dist_063hrcr._.js"],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/app_result_page_1o_1f74.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_recharts_es6_util_0iydtsy._.js","static/chunks/node_modules_recharts_es6_component_0na1-t3._.js","static/chunks/node_modules_recharts_es6_state_0n6pd0s._.js","static/chunks/node_modules_recharts_es6_cartesian_0qtvxb7._.js","static/chunks/node_modules_recharts_es6_170q7k1._.js","static/chunks/node_modules_0h6p3n1._.js","static/chunks/app_result_page_0j9lcyg.js"],
-    source: "dynamic"
-});
